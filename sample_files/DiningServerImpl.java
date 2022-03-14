fbbe891ff65ce85254464ac4b899d1f7351b4867
Joseph Hammana <82621100+Hammana963@users.noreply.github.com>
2022-03-13 20:11:15 -0700

    Update DiningServerImpl.java

--- sample_files/DiningServerImpl.java
+++ sample_files/DiningServerImpl.java
@@ -9,31 +9,42 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class DiningServerImpl  implements DiningServer
-{  
-        // states of philosphers
-        enum State {THINKING, HUNGRY, EATING};
-        
-        public static final int NUM_PHILOSOPHERS = 5;
-        
-        // keeps record of philosphers and current state
-        private State[] state;
-        
-        public DiningServerImpl()
-        {
-                state = new State[NUM_PHILOSOPHERS];
-                
-                // init all philosophers to thinking
-                for (int i = 0; i < NUM_PHILOSOPHERS; i++)
-                        state[i] = State.THINKING;
+public class DiningServerImpl implements DiningServer {
+    ReentrantLock keys[] = new ReentrantLock[5];
+    Condition forks[] = new Condition[5];
+
+    // make 5 keys in the constructor
+    // make 5 chopsticks in the constructor
+    public DiningServerImpl() {
+        for(int i = 0; i < 5; i++) {
+            keys[i] = new ReentrantLock();
+            forks[i] = keys[i].newCondition();
         }
-        
-        public void takeForks(int pnum)  {
-                state[pnum] = State.HUNGRY;
+    }
+
+    //override the takeForks function from DiningServer interface
+    @Override
+    public void takeForks(int philNumber) {
+        //Philosophers try to grab the forks
+        int phil = philNumber + 1;
+        try {
+            //if fork is available, grab it. Otherwise wait
+            System.out.println("Philosopher " + phil + " is trying to grab a fork.");
+            if(keys[philNumber].getHoldCount()>0)
+                forks[philNumber].await();
+
+        }catch (Exception e) {
+            e.printStackTrace();
         }
-        
-        public void returnForks(int pnum) {
-                state[pnum] = State.THINKING;
-        }
-        
+
+    }
+
+    //override the returnForks function from DiningServer interface
+    @Override
+    public void returnForks(int philNumber) {
+        //return the fork if someone needs it
+        if(keys[philNumber].getHoldCount()>0)
+            forks[philNumber].signal();
+    }
+
 }
